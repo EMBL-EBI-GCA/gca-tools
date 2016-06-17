af2c03302efae91cd3796714a54d8ed09743f4b1
peterwharrison <peter@ebi.ac.uk>
2016-06-17 13:26:05 +0100

    Update hPSCreg login codes

--- literature_mining/shell/update_literature_ids.sh
+++ literature_mining/shell/update_literature_ids.sh
@@ -3,4 +3,4 @@
 GCA_TOOLS=/nfs/production/reseq-info/work/rseqpipe/perl_code/gca-tools
 
 perl $GCA_TOOLS/literature_mining/scripts/literature_mining.ebisc.pl -user $IMS_USER -pass $IMS_PASS -outfile /nfs/production/reseq-info/work/rseqpipe/literature_mining_IDs/ebisc.tsv \
-&& perl $GCA_TOOLS/literature_mining/scripts/literature_mining.hpscreg.pl -user $hPSCreg_USER -pass $hPSCreg_PASS -outfile /nfs/production/reseq-info/work/rseqpipe/literature_mining_IDs/hpscreg.tsv
+&& perl $GCA_TOOLS/literature_mining/scripts/literature_mining.hpscreg.pl -user $HPSCREG_USER -pass $HPSCREG_PASS -outfile /nfs/production/reseq-info/work/rseqpipe/literature_mining_IDs/hpscreg.tsv
