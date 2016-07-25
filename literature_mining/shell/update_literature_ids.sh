079b8cea9af7320e2a1cfb2890b077e693fd5b82
peterwharrison <peter@ebi.ac.uk>
2016-07-25 16:42:06 +0100

    Use default servers

--- literature_mining/shell/update_literature_ids.sh
+++ literature_mining/shell/update_literature_ids.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-SERVER1=$1
-
 perl $GCA_TOOLS/literature_mining/scripts/literature_mining.ebisc.pl -user $IMS_USER -pass $IMS_PASS -outfile /nfs/production/reseq-info/work/rseqpipe/literature_mining_IDs/ebisc.tsv \
-&& perl $GCA_TOOLS/literature_mining/scripts/literature_mining.hipsci.pl -es_host=$SERVER1:9200 \
+&& perl $GCA_TOOLS/literature_mining/scripts/literature_mining.hipsci.pl \
+&& perl $GCA_TOOLS/literature_mining/scripts/literature_mining.igsr.pl \
 #&& perl $GCA_TOOLS/literature_mining/scripts/literature_mining.hpscreg.pl -user $HPSCREG_USER -pass $HPSCREG_PASS -outfile /nfs/production/reseq-info/work/rseqpipe/literature_mining_IDs/hpscreg.tsv
